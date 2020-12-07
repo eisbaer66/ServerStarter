d7e31ee492d9e32dae65aa842611d386ee4d7726
eisbaer66 <janweidner87@gmail.com>
2020-12-07 02:38:57 +0100

    name instead of steamid;

---
+++ src/ServerStarter/Client/Identity/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using System.Security.Claims;
+
+namespace ServerStarter.Client.Identity
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetName(this ClaimsPrincipal user)
+        {
+            var claim = user.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name);
+
+            return claim?.Value ?? user.Identity.Name;
+        }
+    }
+}
